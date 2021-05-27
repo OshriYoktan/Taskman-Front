52170d380419b44b44d888d1e95fe90f5c7389a1
OshriYoktan <oshriyok1@gmail.com>
2021-05-27 17:15:48 +0300

    oshris collab

--- src/cmps/TaskModal/TaskModal.jsx
+++ src/cmps/TaskModal/TaskModal.jsx
@@ -262,7 +262,7 @@ export function TaskModal({ taskModalOp }) {
                                     <button onClick={() => onAttRemove(attac._id)}>Delete</button>
                                 </div>
                             </div>
-                            {attNameModal && <div style={{ transform: `translate(-565px,${client.clientY + 400}px)` }} className="att-edit">
+                            {attNameModal && <div style={{ transform: `translate(-540px,${client.clientY - 190}px)` }} className="att-edit">
                                 <div className="att-edit-header">
                                     <p>Edit attachment</p>
                                     <button onClick={() => setAttNameModal(false)}>x</button>
