41e75d95a7c9d3f490b95c2895c18035dc980696
Aviv Zohar <avivzo9@gmail.com>
2021-07-26 14:46:13 +0300

    user length fixes

--- src/cmps/BoardMenu/BoardMenu.jsx
+++ src/cmps/BoardMenu/BoardMenu.jsx
@@ -132,6 +132,8 @@ export function BoardMenu({ boardMenuOp }) {
         const membersLength = []
         await currBoard.members.forEach(async m => {
             const member = await userService.getUserById(m._id)
+            tasksLeng.names.push(member.name)
+            tasksLeng.tasks.push(member.tasks.length)
             membersLength.push(member.tasks.length);
         })
         setTasks(membersLength)
@@ -156,13 +158,7 @@ export function BoardMenu({ boardMenuOp }) {
         });
     }
 
-
     const setStatistics = async () => {
-        await currBoard.members.forEach(async m => {
-            const member = await userService.getUserById(m._id)
-            tasksLeng.names.push(member.name)
-            tasksLeng.tasks.push(member.tasks.length)
-        })
         currBoard.cards.forEach(card => {
             card.tasks.forEach(task => {
                 if (task.doneAt) tasksProgress.completed.push(task)
